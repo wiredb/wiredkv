754958647858345ad9e613a77acb72d7c1d62156
Leon Ding <deen.job@qq.com>
2025-02-22 02:03:02 +0800

    feat: add types package tables search.

--- types/table.go
+++ types/table.go
@@ -1,6 +1,8 @@
 package types
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"gopkg.in/mgo.v2/bson"
+)
 
 // curl -X PUT http://192.168.31.221:2668/tables/key-03 \
 //      -H "Content-Type: application/json" \
@@ -27,9 +29,79 @@ type Tables struct {
 	TTL   uint64         `json:"ttl,omitempty"`
 }
 
+// 新建一个 Tables
+func NewTables() *Tables {
+	return &Tables{
+		Table: make(map[string]any),
+	}
+}
+
+// Clear 清空 Table 和 TTL
 func (tab *Tables) Clear() {
 	tab.TTL = 0
 	tab.Table = make(map[string]any)
+}
+
+// 向 Table 中添加一个项
+func (tab *Tables) AddItem(key string, value any) {
+	tab.Table[key] = value
+}
+
+// 从 Table 中删除一个项
+func (tab *Tables) RemoveItem(key string) {
+	delete(tab.Table, key)
+}
+
+// 检查 Table 中是否包含指定的键
+func (tab *Tables) ContainsKey(key string) bool {
+	_, exists := tab.Table[key]
+	return exists
+}
+
+// 从 Table 中获取一个项
+func (tab *Tables) GetItem(key string) any {
+	if tab.ContainsKey(key) {
+		return tab.Table[key]
+	}
+	return nil
+}
+
+// 从 Tables 查找出键为目标 key 的值，包括所有值中值
+func (tab *Tables) SearchItem(key string) any {
+	var results []any
+	if items, exists := tab.Table[key]; exists {
+		results = append(results, items)
+	}
+
+	for _, item := range tab.Table {
+		if innerMap, ok := item.(map[string]any); ok {
+			results = append(results, searchInMap(innerMap, key)...)
+		}
+	}
+
+	return results
+}
+
+func searchInMap(m map[string]any, key string) []any {
+	var results []any
+	if item, exists := m[key]; exists {
+		results = append(results, item)
+	}
+
+	// 遍历 map，查找是否有嵌套的 map 类型
+	for _, value := range m {
+		if nestedMap, ok := value.(map[string]any); ok {
+			// 递归查找嵌套的 map
+			results = append(results, searchInMap(nestedMap, key)...)
+		}
+	}
+
+	return results
+}
+
+// 获取 Table 中的元素个数
+func (tab *Tables) Size() int {
+	return len(tab.Table)
 }
 
 func (tab Tables) ToBSON() ([]byte, error) {
